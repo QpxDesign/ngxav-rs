970741248aee8b2d3cf97faf9453a6eb1bdcdff1
qpxdesign <qpxdesign@users.noreply.github.com>
2024-02-21 17:32:47 -0500

    made large_files mode give unique requests

--- src/utils/sort_by_body_size.rs
+++ src/utils/sort_by_body_size.rs
@@ -2,17 +2,27 @@
 mod structs;
 use crate::structs::LineParseResult::LineParseResult;
 use rayon::slice::ParallelSliceMut;
+use std::collections::HashMap;
+
 pub fn sort_by_body_size(log_selection: Vec<LineParseResult>, n: usize) {
     let mut parsed_lines = log_selection.clone();
-    parsed_lines.par_sort_by_key(|a: &LineParseResult| a.body_bytes_sent.clone());
-
-    parsed_lines.par_sort_by_key(|a| a.body_bytes_sent.clone());
-    if parsed_lines.len() < n {
-        parsed_lines = parsed_lines[0..parsed_lines.len()].to_vec();
-    } else {
-        parsed_lines = parsed_lines[0..n].to_vec();
-    }
+    let mut occurrences: HashMap<String, LineParseResult> = HashMap::new();
     for line in parsed_lines {
+        if occurrences.contains_key(&line.request) == false {
+            occurrences.insert(line.request.clone(), line);
+        }
+    }
+    let mut final_lines: Vec<_> = occurrences.values().cloned().collect();
+    final_lines.par_sort_by_key(|a: &LineParseResult| a.body_bytes_sent.clone());
+
+    final_lines.par_sort_by_key(|a| a.body_bytes_sent.clone());
+    if final_lines.len() < n {
+        final_lines = final_lines[0..final_lines.len()].to_vec();
+    } else {
+        final_lines = final_lines[0..n].to_vec();
+    }
+
+    for line in final_lines {
         println!(
             "{size_mb} {ip} {host} {request}",
             size_mb = bytes_size_formatter(line.body_bytes_sent),
