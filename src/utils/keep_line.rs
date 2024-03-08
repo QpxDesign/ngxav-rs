a16b327fd9bba8b1f2920620abe1be3deebe2793
qpxdesign <qpxdesign@users.noreply.github.com>
2024-03-08 07:51:58 -0500

    fixed user agent based searchers, drastically speed up test cases

--- src/utils/keep_line.rs
+++ src/utils/keep_line.rs
@@ -50,6 +50,36 @@ pub fn keep_line(parsed_line: &LineParseResult, cm: bool) -> bool {
     {
         return false;
     }
+    if ARGS.browser.is_some()
+        || ARGS.device_category.is_some()
+        || ARGS.os.is_some()
+        || ARGS.bot.is_some()
+    {
+        let parsed_ua = parse_user_agent(parsed_line.user_agent);
+        if ARGS.browser.is_some()
+            && parsed_ua.browser.to_lowercase()
+                != ARGS.browser.as_ref().expect("WOOP").to_lowercase()
+        {
+            return false;
+        }
+        if ARGS.os.is_some()
+            && parsed_ua.operating_system.to_lowercase()
+                != ARGS.os.to_owned().expect("WOOP").to_lowercase()
+        {
+            return false;
+        }
+        if ARGS.device_category.is_some()
+            && ARGS.device_category.as_ref().unwrap().as_str() != parsed_ua.category
+        {
+            return false;
+        }
+        if ARGS.bot.is_some() && ARGS.bot.unwrap() == false && parsed_ua.isBot == true {
+            return false;
+        }
+        if ARGS.bot.is_some() && ARGS.bot.unwrap() == true && parsed_ua.isBot == false {
+            return false;
+        }
+    }
 
     if cm == false {
         return true;
@@ -90,30 +120,5 @@ pub fn keep_line(parsed_line: &LineParseResult, cm: bool) -> bool {
             return false;
         }
     }
-
-    let parsed_ua = parse_user_agent(parsed_line.user_agent);
-    if ARGS.browser.is_some()
-        && parsed_ua.browser.to_lowercase() != ARGS.browser.as_ref().expect("WOOP").to_lowercase()
-    {
-        return false;
-    }
-    if ARGS.os.is_some()
-        && parsed_ua.operating_system.to_lowercase()
-            != ARGS.os.to_owned().expect("WOOP").to_lowercase()
-    {
-        return false;
-    }
-    if ARGS.device_category.is_some()
-        && ARGS.device_category.as_ref().unwrap().as_str() != parsed_ua.category
-    {
-        return false;
-    }
-    if ARGS.bot.is_some() && ARGS.bot.unwrap() == false && parsed_ua.isBot == true {
-        return false;
-    }
-    if ARGS.bot.is_some() && ARGS.bot.unwrap() == true && parsed_ua.isBot == false {
-        return false;
-    }
-
     return true;
 }
